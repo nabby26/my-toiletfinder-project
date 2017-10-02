34c1557eb9cec99cbc4efcd73308503bc958a551
Nancy <s3510210@student.rmit.edu.au>
2017-10-02 19:25:51 +1100

    User datacloud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def show
-  	@user = User.find(params[:id])
+  	@user = User.find params[:id]
   end 
 
   def settings 
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   end 
 
   def create
-    @user = User.new(user_params)
+    @user = User.new user_params
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -31,8 +31,8 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
+    @user = User.find params[:id]
+    if @user.update user_params
       flash[:success] = "Profile updated"
       redirect_to @user
     else
@@ -41,7 +41,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
+    @user = User.find params[:id]
+    @user.destroy
     flash[:success] = "User deleted"
     redirect_to root_url
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,26 @@
+require "google/cloud/datastore"
 class User < ApplicationRecord
+  attr_accessor :id, :name, :email, :password_digest, :admin
+
+  # Return a Google::Cloud::Datastore::Dataset for the configured dataset.
+  # The dataset is used to create, read, update, and delete entity objects.
+  def self.dataset
+    @dataset ||= Google::Cloud::Datastore.new(
+      project: Rails.application.config.database_configuration[Rails.env]["dataset_id"]
+      )
+  end
+
+  # Lookup User by ID.  Returns User or nil.
+  def self.find id
+    query    = Google::Cloud::Datastore::Key.new "User", id.to_i
+    entities = dataset.lookup query
+
+    from_entity entities.first if entities.any?
+  end
+
+  # Add Active Model validation support to User class.
+  include ActiveModel::Validations
+
   before_save { self.email = email.downcase } 
 
   validates :name,  presence: true, length: { maximum: 50 }
@@ -43,6 +65,30 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
   
+  # Save the book to Datastore.
+  # @return true if valid and saved successfully, otherwise false.
+  def save
+    if valid?
+      entity = to_entity
+      User.dataset.save entity
+      self.id = entity.key.id
+      true
+    else
+      false
+    end
+  end
+
+  # Set attribute values from provided Hash and save to Datastore.
+  def update attributes
+    attributes.each do |name, value|
+      send "#{name}=", value if respond_to? "#{name}="
+    end
+    save
+  end
+
+  def destroy
+    User.dataset.delete Google::Cloud::Datastore::Key.new "User", id
+  end
 
 
 end
