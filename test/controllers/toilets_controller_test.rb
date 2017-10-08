250f265611ab1e925178d01da456270e0b554354
nabilaherzegovina <s3463558@student.rmit.edu.au>
2017-10-08 18:04:27 +1100

    There was some merge conflict

--- test/controllers/toilets_controller_test.rb
+++ test/controllers/toilets_controller_test.rb
@@ -15,37 +15,6 @@ class ToiletsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-<<<<<<< HEAD
-  # test "should create toilet" do
-  #   assert_difference('Toilet.count') do
-  #     post toilets_url, params: { toilet: { description: @toilet.description, location: @toilet.location, title: @toilet.title } }
-  #   end
-
-  #   assert_redirected_to toilet_url(Toilet.last)
-  # end
-
-  # test "should show toilet" do
-  #   get toilet_url(@toilet)
-  #   assert_response :success
-  # end
-
-  # test "should get edit" do
-  #   get edit_toilet_url(@toilet)
-  #   assert_response :success
-  # end
-
-  # test "should update toilet" do
-  #   patch toilet_url(@toilet), params: { toilet: { description: @toilet.description, location: @toilet.location, title: @toilet.title } }
-  #   assert_redirected_to toilet_url(@toilet)
-  # end
-
-  # test "should destroy toilet" do
-  #   assert_difference('Toilet.count', -1) do
-  #     delete toilet_url(@toilet)
-  #   end
-  #   assert_redirected_to toilets_url
-  # end
-=======
   test "should create toilet" do
     assert_difference('Toilet.count') do
       post toilets_url, params: { toilet: { description: @toilet.description, disabled_opt: @toilet.disabled_opt, gender_neutral: @toilet.gender_neutral, location: @toilet.location, parentsRoom: @toilet.parentsRoom, title: @toilet.title } }
@@ -76,5 +45,4 @@ class ToiletsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to toilets_url
   end
->>>>>>> 13ede08b82931b0188a27b7737e1d4844e0d5784
 end
